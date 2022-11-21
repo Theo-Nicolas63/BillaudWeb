4cb28e1cfafc3a9d9bc33530c0c1ceec277c0dc1
thnicolas5 <Theo.NICOLAS@etu.uca.fr>
2022-11-21 18:12:15 +0100

    [DEV] page accueil => test de carousel vertical

--- www/js/script.js
+++ www/js/script.js
@@ -1,9 +1,11 @@
 const text = document.querySelector('.transition');
 const text2 = document.querySelector('.transition2');
+const section = document.querySelector('.section');
+const hid_section = document.querySelector('.hid-section');
 
 window.addEventListener('scroll', () => {
 
-    const {scrollTop, clientHeight} = document.documentElement;
+    /* const {scrollTop, clientHeight} = document.documentElement;
 
     // console.log(scrollTop, clientHeight);
 
@@ -19,5 +21,8 @@ window.addEventListener('scroll', () => {
     if(scrollTop < (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8){
         text.classList.remove('active')
         text2.classList.remove('active')
-    }
+    } */
+    section.classList.remove('active');
+    hid_section.classList.add('active');
+
 })
